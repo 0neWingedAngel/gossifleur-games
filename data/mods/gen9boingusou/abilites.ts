d2b8ab33d92f368de9d2d5d328eff66de8ee89b4
0neWingedAngel <lampshadehatwearer@gmail.com>
2024-08-14 20:57:11 -0400

    Update abilites.ts
    Adds Chaos Burst.

--- data/mods/gen9boingusou/abilites.ts
+++ data/mods/gen9boingusou/abilites.ts
@@ -1,6 +1,25 @@
 // bring other abilites not in this file
 export const Abilities: import('../../../sim/dex-abilities').ModdedAbilityDataTable = {
-	// solarize (mega solrock's ability)
+		chaosburst: {
+		onResidualOrder: 29,
+		onResidual(pokemon) {
+			if (pokemon.baseSpecies.baseSpecies !== 'Fecto Elfilis' || pokemon.transformed || !pokemon.hp) return;
+			if (pokemon.species.id === 'chaoselfilis' || pokemon.hp > pokemon.maxhp / 2) return;
+			this.add('-activate', pokemon, 'ability: Power Construct');
+			pokemon.formeChange('Chaos Elfilis', this.effect, true);
+			pokemon.baseMaxhp = Math.floor(Math.floor(
+				2 * pokemon.species.baseStats['hp'] + pokemon.set.ivs['hp'] + Math.floor(pokemon.set.evs['hp'] / 4) + 100
+			) * pokemon.level / 100 + 10);
+			const newMaxHP = pokemon.volatiles['dynamax'] ? (2 * pokemon.baseMaxhp) : pokemon.baseMaxhp;
+			pokemon.hp = newMaxHP - (pokemon.maxhp - pokemon.hp);
+			pokemon.maxhp = newMaxHP;
+			this.add('-heal', pokemon, pokemon.getHealth, '[silent]');
+		},
+		flags: {failroleplay: 1, noreceiver: 1, noentrain: 1, notrace: 1, failskillswap: 1, cantsuppress: 1},
+		name: "Chaos Burst",
+		rating: 5,
+		num: 211,
+	},
 	solarize: {
 		onModifyTypePriority: -1,
 		onModifyType(move, pokemon) {
